4e1d5170f7a4d69200006dcf0c5352e82de34dbb
Greg Kempe <greg@kempe.net>
2014-11-10 16:24:21 +0200

    Allow blank topics

--- fh/models.py
+++ fh/models.py
@@ -3,7 +3,7 @@ from cms.models.pluginmodel import CMSPlugin
 from django.db import models
 
 class DiscourseTopicModel(CMSPlugin):
-    topic = models.CharField(max_length=100, null=True)
+    topic = models.CharField(max_length=100, blank=True, null=True)
 
     def __unicode__(self):
         return self.topic or '(none)'
